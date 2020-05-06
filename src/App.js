384e8f1b9e28c13ced1c474c93f86d2bd6cd46df
Parth Singh <parth.singh71@gmail.com>
2020-05-06 23:34:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Table from "./table";
+import Form from "./form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    characters : [],
+  }
+  render() {
+    const { characters } = this.state
+    return (
+      <div className="App">
+        <h1>Hello React</h1>
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      })
+    })
+  }
+
+
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
+
 }
 
 export default App;
